01c15ece20bdceacca4f59d275d1cff1caa08ac4
Prasanth Varghese <mail2prasanthv@gmail.com>
2023-05-30 22:42:31 +0530

    Step 11:We are adding contacts using  “AddContact” component.Create two state variables in this component to set/track the values of name and email fields. Step 12: When click on the Add button we need to submit the response/ add the details to the state defined in App.js. In this stage we call a user defined function save and log/print the state variable.

--- src/components/AddContact.js
+++ src/components/AddContact.js
@@ -1,18 +1,56 @@
 import React from "react";
 
 class AddContact extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      name: "", //state variable to store name
+      email: "" ////state variable to store email
+    }
+  }
+  //method to set name  - calls onchange of name field
+  setName = (namearg) => {
+    this.setState({
+      name: namearg
+    })
+  }
+   //method to set name - calls onchange of email field
+   setEmail = (emailarg) => {
+    this.setState({
+      email: emailarg
+    })
+  }
+
+  save = (e) => { // userdefined function for validation and saving the data in the state variable(contact) in App.js(later)
+    e.preventDefault();
+    if(this.state.name==="" || this.state.email ==="") {
+      alert("All the fields are mandatory")
+      return;
+    }
+    console.log(this.state.name)
+    console.log(this.state.email)
+  }
+  
   render() {
     return (
       <div>
         <h2>Add Contact</h2>
-        <form>
+        <form onSubmit={this.save}>
           <div>
             <label>Name</label>
-            <input type="text" placeholder="Type Name here"></input>
+            <input  type="text" 
+                    placeholder="Type Name here" 
+                    onChange={(e) => this.setName(e.target.value)} 
+                    value={this.state.name}>
+            </input>
           </div>
           <div>
             <label>Email</label>
-            <input type="text" placeholder="Type Email here"></input>
+            <input  type="text" 
+                    placeholder="Type Email here" 
+                    onChange={(e) =>  this.setEmail(e.target.value)} 
+                    value={this.state.email}>
+            </input>
           </div>
           <button>Add Contact</button>
         </form>
