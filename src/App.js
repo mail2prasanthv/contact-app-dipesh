68ffb76737fb8d6d9bfda1de735f157beea56f88
Prasanth Varghese <mail2prasanthv@gmail.com>
2023-06-01 07:11:58 +0530

    In the above example, we store data in the session and it  loses if we refresh the page.For permanent storage of we can use the localstorage of the browser.. We need a function which saves data in the localstorage whenever there is a change in the state variable ‘contactlist’ . Thi type of method is called useEffect(). A component can have multiple useEffect. One useEffect() to store the data in localstorage whenever there is a change in the state variable ‘contactlist’. And other one is to retrieve from the locastorage and restore in ‘contactlist’ when we refresh the page.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import AddContact from "./components/AddContact";
 import Header from "./components/Header";
 import ContactList from "./components/ContactList";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
+  const LOCAL_STOTAGE_KEY="CONTACTS_LS"
 
    //initializing empty array as contactlist state variable
   const[contactlist, setContactlist] = useState([]);
@@ -14,6 +15,12 @@ function App() {
     //And adding the new one
       setContactlist([...contactlist, contact]);
   }
+
+  //method to capture changes in the array contactlist and save it to the localstorage with key LOCAL_STOTAGE_KEY
+  useEffect(()=>{
+    localStorage.setItem(LOCAL_STOTAGE_KEY, JSON.stringify(contactlist));
+  }, [contactlist])
+
   return (
     <div>
       <Header/>
